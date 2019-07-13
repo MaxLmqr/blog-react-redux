f1e608f7e4bb826488846aa4a58b26163da0d85f
Max Lmqr <max.lemasquerier@protonmail.com>
2019-07-13 16:44:11 +0200

    end

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,46 @@
+import _ from "lodash";
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
 export const fetchPosts = () => {
-  return {
-    type: "FETCH_POSTS"
+  return async dispatch => {
+    const response = await jsonPlaceholder.get("/posts");
+
+    dispatch({
+      type: "FETCH_POSTS",
+      payload: response.data
+    });
   };
 };
+
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: "FETCH_USER", payload: response.data });
+};
+
+export const fetchPostsAndUsers = function() {
+  return async function(dispatch, getState) {
+    await dispatch(fetchPosts());
+
+    // const userIds = _.uniq(_.map(getState().posts, "userId"));
+    // userIds.forEach(id => dispatch(fetchUser(id)));
+
+    // Fais la meme chose que ce qui est commenté ci dessus mais plus factorisé !
+    _.chain(getState().posts)
+      .map("userId")
+      .uniq()
+      .forEach(id => dispatch(fetchUser(id)))
+      .value();
+  };
+};
+
+// version utilisant MEMOIZE
+// export const fetchUser = id => dispatch => {
+//   _fetchUser(id, dispatch);
+// };
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from "redux";
+import postsReducer from "./postsReducer";
+import usersReducer from "./usersReducer";
 
 export default combineReducers({
-  dummy: () => null
+  posts: postsReducer,
+  users: usersReducer
 });
